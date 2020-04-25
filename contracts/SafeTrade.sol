25f4304a6fdb92904bdc1c502b55f02a74c72936
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2020-04-25 13:32:32 +0800

    add most functionalities, revamped design

--- contracts/SafeTrade.sol
+++ contracts/SafeTrade.sol
@@ -8,29 +8,50 @@ contract SafeTrade {
         uint256 price;
         address buyer;
         bool isReserved;
+        address seller;
+        string img;
+        bool isPurchased;
     }
     uint256 public itemCount;
+
     mapping(uint256 => Item) public items;
     event buyEvent(uint256 indexed _itemId);
+    event addItemEvent(uint256 indexed _itemId);
 
-    constructor() public {
-        addItem("item1", 100);
-        addItem("item2", 200);
-    }
-
-    function addItem(string _name, uint256 _price) private {
+    function addItem(string _name, uint256 _price, string _img) public {
         itemCount++;
-        items[itemCount] = Item(itemCount, _name, _price, 0x0, false);
+        items[itemCount] = Item(
+            itemCount,
+            _name,
+            _price,
+            0x0,
+            false,
+            msg.sender,
+            _img,
+            false
+        );
+        emit addItemEvent(itemCount);
     }
 
-    function buy(uint256 _itemId) public {
+    function buy(uint256 _itemId) public payable {
+        require(
+            msg.sender != items[_itemId].seller,
+            "Cannot buy your own item"
+        );
         require(items[_itemId].isReserved == false, "Item is reserved");
         require(_itemId > 0 && _itemId <= itemCount, "Invalid item id");
-        // update item reserved state
         items[_itemId].isReserved = true;
-        // record who buy
         items[_itemId].buyer = msg.sender;
         emit buyEvent(_itemId);
     }
-}
 
+    function confirm(uint256 _itemId) public payable {
+        require(msg.sender == items[_itemId].seller, "Invalid authorization");
+        require(
+            items[_itemId].buyer != 0x0 && items[_itemId].isReserved == true,
+            "No buyer yet"
+        );
+        items[_itemId].isPurchased = true;
+        msg.sender.transfer(address(this).balance);
+    }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -41,7 +41,19 @@ App = {
                         toBlock: "latest",
                     }
                 )
-                .watch(function (error, event) {
+                .watch(function (err, event) {
+                    console.log("event triggered", event);
+                    App.render();
+                });
+            instance
+                .addItemEvent(
+                    {},
+                    {
+                        fromBlock: 0,
+                        toBlock: "latest",
+                    }
+                )
+                .watch(function (err, event) {
                     console.log("event triggered", event);
                     App.render();
                 });
@@ -71,57 +83,110 @@ App = {
             .then(function (itemCount) {
                 var itemListing = $("#itemListing");
                 itemListing.empty();
-
-                var itemSelect = $("#itemSelect");
-                itemSelect.empty();
-
-                for (var i = 1; i <= itemCount; i++) {
+                for (var i = 1; i <= itemCount.toNumber(); i++) {
                     safeTradeInstance.items(i).then(function (item) {
                         var id = item[0];
                         var name = item[1];
                         var itemPrice = item[2];
                         var buyer = item[3];
                         var isReserved = item[4];
+                        var seller = item[5];
+                        var img = item[6];
+                        var isPurchased = item[7];
+                        if (!isPurchased) {
+                            var buyButton = document.createElement("button");
+                            buyButton.innerHTML = "Buy";
+                            buyButton.className = "btn btn-primary";
+                            buyButton.style.cssText = "margin-right:10px;";
+                            buyButton.addEventListener("click", function () {
+                                App.contracts.SafeTrade.deployed()
+                                    .then(function (instance) {
+                                        return instance.buy(id, {
+                                            from: App.account,
+                                            value: web3.toWei(
+                                                itemPrice,
+                                                "ether"
+                                            ),
+                                        });
+                                    })
+                                    .then(function (res) {
+                                        content.hide();
+                                        loader.show();
+                                    })
+                                    .catch(function (err) {
+                                        console.error(err);
+                                    });
+                            });
 
-                        var itemTemplate =
-                            "<tr><th>" +
-                            id +
-                            "</th><td>" +
-                            name +
-                            "</td><td>" +
-                            `<img src='http://placekitten.com/200/150?image=${id}'` +
-                            "</td><td>" +
-                            "SGD " +
-                            itemPrice +
-                            "</td><td>" +
-                            isReserved +
-                            "</td></tr>";
-                        itemListing.append(itemTemplate);
+                            var dealButton = document.createElement("button");
+                            dealButton.innerHTML = "Deal";
+                            dealButton.className = "btn btn-primary";
+                            dealButton.addEventListener("click", function () {
+                                App.contracts.SafeTrade.deployed()
+                                    .then(function (instance) {
+                                        return instance.confirm(id, {
+                                            from: App.account,
+                                        });
+                                    })
+                                    .then(function (res) {
+                                        $("#content").hide();
+                                        $("#loader").show();
+                                    })
+                                    .then(function (err) {
+                                        console.error(err);
+                                    });
+                            });
+                            var itemTemplate = document.createElement("tr");
+                            var _id = document.createElement("th");
+                            _id.innerHTML = id;
+                            var _name = document.createElement("td");
+                            _name.innerHTML = name;
+                            var _img = document.createElement("td");
+                            var _img2 = document.createElement("img");
+                            _img2.setAttribute("src", img);
+                            _img.append(_img2);
+                            var _itemPrice = document.createElement("td");
+                            _itemPrice.innerHTML = itemPrice;
+                            var _isReserved = document.createElement("td");
+                            _isReserved.innerHTML = isReserved;
+                            var _buyer = document.createElement("td");
+                            _buyer.innerHTML = buyer;
+                            var _seller = document.createElement("td");
+                            _seller.innerHTML = seller;
 
-                        var itemOption =
-                            "<option value='" +
-                            id +
-                            "' >" +
-                            name +
-                            "</ option>";
-                        itemSelect.append(itemOption);
+                            itemTemplate.append(_id);
+                            itemTemplate.append(_name);
+                            itemTemplate.append(_img);
+                            itemTemplate.append(_itemPrice);
+                            itemTemplate.append(_isReserved);
+                            itemTemplate.append(_buyer);
+                            itemTemplate.append(_seller);
+                            itemTemplate.append(buyButton);
+                            itemTemplate.append(dealButton);
+
+                            itemListing.append(itemTemplate);
+                        }
                     });
                 }
                 loader.hide();
                 content.show();
             })
-            .catch(function (error) {
-                console.warn(error);
+            .catch(function (err) {
+                console.warn(err);
             });
     },
 
-    buyItem: function () {
-        var itemId = $("#itemSelect").val();
+    addItem: function () {
+        var itemName = $("#itemName").val();
+        var itemPrice = $("#itemPrice").val();
+        var imgLink = $("#itemImg").val() ? $("#itemImg").val() : "";
         App.contracts.SafeTrade.deployed()
             .then(function (instance) {
-                return instance.buy(itemId, {from: App.account});
+                return instance.addItem(itemName, itemPrice, imgLink, {
+                    from: App.account,
+                });
             })
-            .then(function (result) {
+            .then(function (res) {
                 $("#content").hide();
                 $("#loader").show();
             })
